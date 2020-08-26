d59f4441e1a417e8c0f1439d69611db4e45d5428
Angelo Yenque T <ayenquet@gmail.com>
2020-08-26 17:05:06 -0500

    Actualizando routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,16 @@ Rails.application.routes.draw do
 	get 'static/contactus'
 	get 'static/services'
 	get 'static/faq'
-<<<<<<< HEAD
+
 	get 'static/busquedacitas'
 	get 'static/proforma'
 	get 'static/separardepartamento'
-=======
+
 	get 'static/detalledepartamento'
 	get 'static/solicitarcita'
 
 	get 'static/departamentos'
->>>>>>> 12de0502f8ef753c422f488ce5d4d8685a186f74
+
 
 	root 'home#index'
 	

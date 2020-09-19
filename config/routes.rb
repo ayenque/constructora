810f4a7a18d241722a3aa6625122f071df72f39a
Wilmer Rivera <wilmmer.rivera@gmail.com>
2020-09-18 23:27:13 -0500

    Cambios y mejoras en las vistas inicio, nosotros, contacto, proyecto, departamentos

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :departamentos do
     member do
 		get :delete
+		
 	  end
    end
 
@@ -62,5 +63,7 @@ Rails.application.routes.draw do
 	root 'home#index'
 
 	post 'static/mimetodo'
+	
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
